56dab5c03a2d08207b6081aee038944d9d1db357
JStern95 <JSternberg95@gmail.com>
2018-11-08 11:43:36 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,23 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+
+  const renderActor =(actor) =>{
+    return (
+      <React.Fragment>
+      <h2>Name: {actor.name}</h2>
+      <p>Movies:</p>
+      <ul>
+        {actor.movies.map(movie=><li>{movie}</li>)}
+      </ul>
+      </React.Fragment>
+    )
+  }
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(actor=><div>{renderActor(actor)}</div>)}
     </div>
   );
 };
